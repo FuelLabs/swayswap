9ad603c803b9d660aa7062bff57672058d12539c
Chris O'Brien <obrchr96@gmail.com>
2022-03-01 19:05:25 -0600

    added svgs, removed some clutter

--- client/components/Swap.tsx
+++ client/components/Swap.tsx
@@ -1,7 +1,5 @@
 import { useContext, Fragment, useState, useEffect } from 'react'
 import { RiSettings3Fill } from 'react-icons/ri'
-import Image from 'next/image'
-import ethLogo from "/public/assets/eth.png"
 
 const style = {
     wrapper: `w-screen flex flex-1 items-center justify-center mb-14`,
@@ -15,13 +13,6 @@ const style = {
     currencySelector: `flex w-1/4`,
     currencySelectorMenuButton: `inline-flex justify-around w-full px-4 py-2 text-sm font-medium text-white 
     bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`,
-    menuWrapper: ``,
-    currencySelectorContent: `w-full h-min flex justify-between items-center bg-[#2D2F36] 
-    hover:bg-[#41444F] rounded-2xl text-xl font-medium cursor-pointer p-2 mt-[-0.2rem]`,
-    currencySelectorIcon: `flex items-center`,
-    // currencySelectorMenuItems: `absolute w-full mt-2 bg-[#2D2F36] divide-gray-100 
-    // rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`,
-    currencySelectorItem: `flex justify-around rounded-md w-full px-2 py-2 text-sm`,
     
     confirmButton: `bg-[#58c09b] my-2 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center
     justify-center cursor-pointer border border-[#58c09b] hover:border-[#234169]`,
@@ -38,25 +29,8 @@ const Swap = () => {
 
         // sendTransaction()
     }
-
     const assets = require('../lib/assets.json');
-
     const [currentlySelectedCoin, setCurrentlySelectedCoin] = useState("ETH");
-    // const assets =[
-    //     {
-    //         "name": "ETH",
-    //         "color": "CoinETH",
-    //         "amount": 0,
-    //         "img": "https://i.imgur.com/mC6aU4s.png"
-    //     },
-    //     {
-    //         "name": "DAI",
-    //         "color": "CoinDAI",
-    //         "amount": 0,
-    //         "img": "https://i.imgur.com/DRC4jp6.png"
-    //     }
-    // ]
-    
     
   return (
     <div className={style.wrapper}>
@@ -80,19 +54,16 @@ const Swap = () => {
                 {/* coin selector */}
                 <div className={style.currencySelector}>
                     <div className="relative inline-block w-full">
-                        <div>
                         <select className={`${style.currencySelectorMenuButton}`} onChange={(e) => setCurrentlySelectedCoin(e.target.value)}>
-                            
-                            {assets.map(x =>(
-                                <option 
+                            {assets.map(x => (
+                                <option
                                 selected={currentlySelectedCoin === x.name} 
                                 value={x.name}>
-                                    {x.name}
+                                    {x.name} <img src={x.img} />
                                 </option>
                             ))}
-                        </select>  
-                        </div>
-                    </div>  
+                        </select>
+                    </div>
                 </div>
             </div>
 
