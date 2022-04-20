93b384c0dc9920e945a2d23a7d45447f2f682c39
Luiz Estacio <luizstacio@gmail.com>
2022-04-20 16:22:59 +0200

    feat: add seedWallet

--- client/deploy-contracts/index.ts
+++ client/deploy-contracts/index.ts
@@ -4,8 +4,8 @@
 
 // TODO: Remove this file after `forc` enabled deploy a contract to a custom url
 // https://github.com/FuelLabs/sway/issues/1308
-import { seedWallet } from '@fuel-ts/wallet/dist/test-utils';
-import { ContractFactory, NativeAssetId, Wallet, ZeroBytes32 } from "fuels";
+import { hexlify, parseUnits } from "ethers/lib/utils";
+import { ContractFactory, NativeAssetId, ScriptTransactionRequest, Wallet, ZeroBytes32 } from "fuels";
 import path from "path";
 import fs from "fs";
 // @ts-ignore
@@ -15,13 +15,36 @@ const tokenPath = path.join(__dirname, '../../contracts/token_contract/out/debug
 const contractPath = path.join(__dirname, '../../contracts/swayswap_contract/out/debug/swayswap_contract.bin');
 const providerUrl = process.env.REACT_APP_FUEL_PROVIDER_URL || 'https://node.swayswap.io/graphql';
 
+const genBytes32 = () =>
+  hexlify(new Uint8Array(32).map(() => Math.floor(Math.random() * 256)));
+
+export const seedWallet = async (wallet: Wallet) => {
+    const transactionRequest = new ScriptTransactionRequest({
+      gasPrice: 0,
+      gasLimit: "0x0F4240",
+      script: "0x24400000",
+      scriptData: genBytes32()
+    });
+    // @ts-ignore
+    transactionRequest.addCoin({
+      id: "0x000000000000000000000000000000000000000000000000000000000000000000",
+      assetId: NativeAssetId,
+      amount: parseUnits(".5", 9),
+      owner: "0xf1e92c42b90934aa6372e30bc568a326f6e66a1a0288595e6e3fbd392a4f3e6e",
+    });
+    transactionRequest.addCoinOutput(wallet.address, parseUnits(".5", 9), NativeAssetId);
+    const submit = await wallet.sendTransaction(transactionRequest);
+
+    return submit.wait();
+}
+
 export async function deployContract(contextLog: string, binaryPath: string, abi: any) {
     console.log(contextLog, 'Create wallet...');
     console.log(contextLog, 'connected to', providerUrl)
     const wallet = Wallet.generate({ provider: providerUrl });
     
     console.log(contextLog, 'Funding wallet with some coins');
-    await seedWallet(wallet, [[5_000_000, NativeAssetId]]);
+    await seedWallet(wallet);
 
     // Deploy
     console.log(contextLog, 'Load contract binary...');
