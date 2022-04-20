58b2c9d74af3976e6f15af7d3616d4d20283c264
Luiz Estacio <luizstacio@gmail.com>
2022-04-20 13:32:29 +0200

    feat: add contracts configs to CI and update dependencies

--- client/src/context/WalletContext.tsx
+++ client/src/context/WalletContext.tsx
@@ -59,7 +59,7 @@ export const WalletProvider = ({ children }: PropsWithChildren<{}>) => {
           transactionRequest.addCoin({
             id: "0x000000000000000000000000000000000000000000000000000000000000000000",
             assetId: CoinETH,
-            amount: parseUnits(".5", 9),
+            amount: parseUnits(".2", 9),
             owner: "0xf1e92c42b90934aa6372e30bc568a326f6e66a1a0288595e6e3fbd392a4f3e6e",
           });
           transactionRequest.addCoinOutput(wallet.address, parseUnits(".5", 9), CoinETH);
