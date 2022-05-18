40b7a389ab2e96c84624eba0b69dd7b04afbb737
Pedro Nauck <pedronauck@gmail.com>
2022-05-17 21:25:29 -0300

    refac: add animation on invert button

--- client/src/components/InvertButton.tsx
+++ client/src/components/InvertButton.tsx
@@ -1,13 +1,39 @@
-import { AiOutlineArrowDown } from "react-icons/ai";
+import { useState } from "react";
+import { BsArrowDown, BsArrowUp } from "react-icons/bs";
+import cx from "classnames";
 
 const style = {
-  confirmButton: `rounded-2xl font-semibold p-1 border border-[#20242A] bg-[#191B1F] border-4 rounded-2xl cursor-pointer`,
+  confirmButton: `relative flex items-center rounded-2xl font-semibold w-12 h-12
+    border border-[#20242A] bg-[#191B1F] border-4 rounded-2xl cursor-pointer
+    text-gray-500 hover:text-gray-50`,
+  icon: `transition-all`,
+  iconLeft: `translate-x-[4px]`,
+  iconRight: `translate-x-[-4px]`,
+  rotate: `rotate-180`,
 };
 
 export function InvertButton({ onClick }: { onClick: () => void }) {
+  const [isInverted, setInverted] = useState(false);
+
+  function handleClick() {
+    setInverted((s) => !s);
+    onClick?.();
+  }
+
   return (
-    <div onClick={onClick} className={style.confirmButton}>
-      <AiOutlineArrowDown size={32} />
+    <div onClick={handleClick} className={style.confirmButton}>
+      <BsArrowDown
+        size={24}
+        className={cx(style.icon, style.iconLeft, {
+          [style.rotate]: isInverted,
+        })}
+      />
+      <BsArrowUp
+        size={24}
+        className={cx(style.icon, style.iconRight, {
+          [style.rotate]: isInverted,
+        })}
+      />
     </div>
   );
 }
