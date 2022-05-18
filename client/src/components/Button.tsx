d6e673272e84df7f7de087d604bfd7f7a80d77b3
Pedro Nauck <pedronauck@gmail.com>
2022-05-18 02:33:25 -0300

    refac: add global Button component

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,40 @@
+import { useButton } from "@react-aria/button";
+import { AriaButtonProps } from "@react-types/button";
+import { useRef } from "react";
+import cx from "classnames";
+
+export type ButtonProps = AriaButtonProps<"button"> & {
+  className?: string;
+  size?: "sm" | "md" | "lg";
+  variant?: "base" | "primary" | "ghost";
+};
+
+export function Button({
+  size = "sm",
+  variant = "base",
+  ...props
+}: ButtonProps) {
+  const ref = useRef<HTMLButtonElement | null>(null);
+  const { buttonProps, isPressed } = useButton(props, ref);
+
+  const classes = cx({
+    button: true,
+    "button--sm": size === "sm",
+    "button--md": size === "md",
+    "button--lg": size === "lg",
+    "button--base": variant === "base",
+    "button--primary": variant === "primary",
+    "button--ghost": variant === "ghost",
+  });
+
+  return (
+    <button
+      ref={ref}
+      {...buttonProps}
+      className={classes}
+      aria-pressed={isPressed}
+    >
+      {props.children}
+    </button>
+  );
+}
