e250eafc95aee40b547daf43903def8af746974a
Luiz Estacio <luizstacio@gmail.com>
2022-05-19 00:21:40 -0300

    fix: update icons url

--- client/src/lib/CoinsMetadata.ts
+++ client/src/lib/CoinsMetadata.ts
@@ -1,12 +1,18 @@
 import { Coin } from 'src/types';
 import { CONTRACT_ID, TOKEN_ID } from 'src/config';
+import { relativeUrl } from './utils';
+
+export const ASSET_404 = {
+  name: '404',
+  img: relativeUrl('/icons/other.svg'),
+};
 
 const CoinsMetadata: Array<Coin> = [
   {
     name: 'Ether',
     symbol: 'ETH',
     assetId: '0x0000000000000000000000000000000000000000000000000000000000000000',
-    img: '/icons/eth.svg',
+    img: relativeUrl('/icons/eth.svg'),
   },
   {
     name: 'DAI',
@@ -15,7 +21,7 @@ const CoinsMetadata: Array<Coin> = [
     // Make temporarily easy to change token contract id
     // https://github.com/FuelLabs/swayswap-demo/issues/33
     assetId: TOKEN_ID,
-    img: '/icons/dai.svg',
+    img: relativeUrl('/icons/dai.svg'),
   },
   {
     name: 'Sway',
@@ -24,7 +30,7 @@ const CoinsMetadata: Array<Coin> = [
     // Make temporarily easy to change token contract id
     // https://github.com/FuelLabs/swayswap-demo/issues/33
     assetId: CONTRACT_ID,
-    img: '/icons/sway.svg',
+    img: relativeUrl('/icons/sway.svg'),
   },
 ];
 
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,5 +1,11 @@
+import urljoin from 'url-join';
+
+const { PUBLIC_URL } = process.env;
+
 export const objectId = (value: string) => ({
   value,
 });
 
 export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const relativeUrl = (path: string) => urljoin(PUBLIC_URL, path);
