9119e7b5b295edb04349ff97c67ad63651d7bce3
Pedro Nauck <pedronauck@gmail.com>
2022-05-19 18:56:28 -0300

    chore: improve Menu component

--- packages/app/src/components/Menu.tsx
+++ packages/app/src/components/Menu.tsx
@@ -1,10 +1,18 @@
 import { useMenu, useMenuItem } from "@react-aria/menu";
-import { Item } from "@react-stately/collections";
+import { mergeProps } from "@react-aria/utils";
+import { Item as RootItem } from "@react-stately/collections";
 import type { TreeProps, TreeState } from "@react-stately/tree";
 import { useTreeState } from "@react-stately/tree";
 import type { ItemProps, Node } from "@react-types/shared";
+import cx from "classnames";
 import type { FC, Key } from "react";
 import { useEffect, useRef } from "react";
+
+const style = {
+  menu: `rounded-md shadow-sm min-w-[120px] text-gray-300`,
+  menuItem: `flex items-center gap-2 px-4 py-2 focus-ring cursor-pointer text-sm first:rounded-tl-md first:rounded-tr-md
+  last:rounded-bl-md last:rounded-br-md`,
+};
 
 type ListItemProps = {
   item: Node<any>;
@@ -24,16 +32,26 @@ function ListItem({ item, state, onAction, className }: ListItemProps) {
       key: item.key,
       isDisabled,
       onAction,
+      closeOnSelect: true,
     },
     state,
     ref
   );
 
+  const customProps = {
+    onClick: item.props.onPress,
+    onKeyDown: (e: any) => {
+      if (e.keyCode === 32 || e.key === "Enter") {
+        item.props.onPress?.();
+      }
+    },
+  };
+
   return (
     <li
-      {...menuItemProps}
+      {...mergeProps(customProps, menuItemProps)}
       ref={ref}
-      className={className}
+      className={cx(style.menuItem, className)}
       data-focused={isFocused}
     >
       {item.rendered}
@@ -48,7 +66,7 @@ export type MenuProps = TreeProps<any> & {
 };
 
 type MenuComponent = FC<MenuProps> & {
-  Item: typeof CustomItem;
+  Item: typeof Item;
 };
 
 export const Menu: MenuComponent = ({
@@ -71,7 +89,7 @@ export const Menu: MenuComponent = ({
   }, []);
 
   return (
-    <ul {...menuProps} ref={ref} className={className}>
+    <ul {...menuProps} ref={ref} className={cx(style.menu, className)}>
       {[...state.collection].map((item) => (
         <ListItem
           className={item.props.className}
@@ -85,6 +103,8 @@ export const Menu: MenuComponent = ({
   );
 };
 
-type CustomItemComponent = FC<ItemProps<any> & { className?: string }>;
-const CustomItem = Item as CustomItemComponent;
-Menu.Item = CustomItem;
+type ItemComponent = FC<
+  ItemProps<any> & { className?: string; onPress?: () => void }
+>;
+const Item: ItemComponent = RootItem;
+Menu.Item = Item;
