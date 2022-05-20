fdfb1a50c2e80ac3967a77bfcae012c5305c80e5
Pedro Nauck <pedronauck@gmail.com>
2022-05-20 13:36:59 -0300

    refac: remove recoil and use jotai (#166)

--- packages/app/src/lib/recoilEffects.ts
+++
@@ -1,8 +0,0 @@
-import { recoilPersist } from 'recoil-persist';
-
-import { LocalStorageKey } from './constants';
-
-export const persistEffect = recoilPersist({
-  key: `${LocalStorageKey}-state`,
-  storage: localStorage,
-}).persistAtom;
