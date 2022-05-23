c070bdd6b65fdfec6af26802c47b6256def6e8f2
Matt Auer <mattauer@umich.edu>
2022-05-23 17:26:52 -0400

    refactor: move queryClient into its own file

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,5 +1,4 @@
-import toast from "react-hot-toast";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 
 import AppRoutes from "./AppRoutes";
@@ -8,30 +7,9 @@ import { Toaster } from "./components/Toaster";
 
 import { AppContextProvider } from "~/context/AppContext";
 
-const { PUBLIC_URL } = process.env;
+import { queryClient } from "./queryClient";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      onError: (error) => {
-        toast.error((error as Error).message);
-      },
-      // These two are annoying during development
-      retry: false,
-      refetchOnWindowFocus: false,
-      // This is disabled because it causes a bug with arrays with named keys
-      // For example, if a query returns: [BN, BN, a: BN, b: BN]
-      // with this option on it will be cached as: [BN, BN]
-      // and break our code
-      structuralSharing: false,
-    },
-    mutations: {
-      onError: (error) => {
-        toast.error((error as Error).message);
-      },
-    },
-  },
-});
+const { PUBLIC_URL } = process.env;
 
 export default function App() {
   return (
