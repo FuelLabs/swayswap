cd299dcf58370c712b678ea3cf9ad5b8620eef91
Pedro Nauck <pedronauck@gmail.com>
2022-05-25 00:53:06 -0300

    chore: vite config

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,19 +1,11 @@
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: ['es2020'],
   },
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
