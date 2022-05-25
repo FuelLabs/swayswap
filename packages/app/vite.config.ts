122f220d59d4f88132e0a3bbae00f36ceaf85e51
Pedro Nauck <pedronauck@gmail.com>
2022-05-25 18:26:53 -0300

    fix: load right env file inside vite config

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,9 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { config } from 'dotenv';
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-config();
+config({
+  path:
+    process.env.NODE_ENV === 'PRODUCTION'
+      ? resolve(__dirname, './.env.production')
+      : resolve(__dirname, './.env'),
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +20,12 @@ export default defineConfig({
     target: ['es2020'],
     outDir: process.env.BUILD_PATH || 'dist',
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] },
+    }),
+    tsconfigPaths(),
+  ],
   define: {
     'process.env': process.env,
   },
