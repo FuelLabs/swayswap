9418488c38f22a02ac65148ba57f09508929f4b4
Luiz Estacio <luizstacio@gmail.com>
2022-06-01 22:20:10 -0300

    fix: base url

--- packages/app/src/lib/utils.ts
+++ packages/app/src/lib/utils.ts
@@ -1,5 +1,7 @@
 import { urlJoin } from 'url-join-ts';
 
+const { PUBLIC_URL } = process.env;
+
 export const objectId = (value: string) => ({
   value,
 });
@@ -9,7 +11,7 @@ export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve
 
 // url-join-ts auto implements base url
 // but we should wrap urls using it to correct use PUBLIC_URL
-export const relativeUrl = (path: string) => urlJoin(path);
+export const relativeUrl = (path: string) => urlJoin(window.location.origin, PUBLIC_URL, path);
 
 export function omit<T>(list: string[], props: T) {
   return Object.entries(props).reduce((obj, [key, value]) => {
