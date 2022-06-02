c6d3c4aa3adbb2de35f63f4093330b07c28d28c8
Luiz Estacio <luizstacio@gmail.com>
2022-06-02 02:55:23 -0300

    chore

--- .github/workflows/scripts/install-forc.sh
+++ .github/workflows/scripts/install-forc.sh
@@ -40,6 +40,6 @@ get_architecture() {
 get_architecture
 echo "$PLATFORM"
 echo "Download forc"
-curl -sL https://github.com/FuelLabs/sway/releases/download/v0.14.5/forc-binaries-$PLATFORM.tar.gz | tar -xvzf
+# curl -sL https://github.com/FuelLabs/sway/releases/download/v0.14.5/forc-binaries-$PLATFORM.tar.gz | tar -xvzf
 echo "Unpack forc"
-forc-binaries/forc --version
+# forc-binaries/forc --version
