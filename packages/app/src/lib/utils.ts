b8ed19d233f1b6be62a3ab481994468cf0e64ff1
Luiz Estacio <luizstacio@gmail.com>
2022-06-01 22:23:45 -0300

    chore: remove comment

--- packages/app/src/lib/utils.ts
+++ packages/app/src/lib/utils.ts
@@ -9,8 +9,6 @@ export const objectId = (value: string) => ({
 // eslint-disable-next-line no-promise-executor-return
 export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// url-join-ts auto implements base url
-// but we should wrap urls using it to correct use PUBLIC_URL
 export const relativeUrl = (path: string) => urlJoin(window.location.origin, PUBLIC_URL, path);
 
 export function omit<T>(list: string[], props: T) {
