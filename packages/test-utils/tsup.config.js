20ef48fba5ac1e55b637f20acfb5fa29796339f5
Pedro Nauck <pedronauck@gmail.com>
2022-06-07 18:16:53 -0300

    chore: update deps

--- packages/test-utils/tsup.config.js
+++ packages/test-utils/tsup.config.js
@@ -1,8 +1,9 @@
-import baseConfig from '@fuels-ui/config/tsup';
+import baseConfig from '@swayswap/config/tsup';
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
   ...baseConfig(options),
   external: ['react'],
   entry: ['src/index.ts'],
+  treeshake: true,
 }));
