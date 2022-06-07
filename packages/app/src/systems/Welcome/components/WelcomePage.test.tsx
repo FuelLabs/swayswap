72da51d6e417daf84b49a766c03afb2480ab7aca
Pedro Nauck <pedronauck@gmail.com>
2022-06-07 20:35:47 -0300

    refactor: improve welcome tests

--- packages/app/src/systems/Welcome/components/WelcomePage.test.tsx
+++ packages/app/src/systems/Welcome/components/WelcomePage.test.tsx
@@ -1,20 +1,52 @@
 import "cross-fetch/polyfill";
-import { renderWithRouter } from "@swayswap/test-utils";
-
-import { testCheckFirstLoad, testCreateWallet } from "../__tests__/tests";
+import { renderWithRouter, screen } from "@swayswap/test-utils";
 
 import App from "~/App";
 
 describe("WalletPage", () => {
-  it("when first loaded, any route will redirect user to Create Wallet page", async () => {
-    await renderWithRouter(<App />, { route: "/pool/list" });
-    await testCheckFirstLoad();
+  it("should always be redirect to welcome", async () => {
+    renderWithRouter(<App />, { route: "/pool/list" });
+    expect(await screen.findByText(/Welcome to SwaySwap/)).toBeInTheDocument();
   });
 
-  it("should do create wallet flow", async () => {
-    await renderWithRouter(<App />, {
+  it("should be able to do all welcome steps and see swap page after", async () => {
+    const { user } = renderWithRouter(<App />, {
       route: "/welcome/create-wallet",
     });
-    await testCreateWallet();
+
+    /**
+     * First step: Creating wallet
+     */
+    const createWalletBtn = screen.getByRole("button", {
+      name: /Create Wallet/i,
+    });
+    expect(createWalletBtn).toBeInTheDocument();
+    await user.click(createWalletBtn);
+
+    /**
+     * Second step: Adding funds
+     */
+    const addFundsBtn = await screen.findByRole("button", {
+      name: /Give me ETH/i,
+    });
+    expect(addFundsBtn).toBeInTheDocument();
+    await user.click(addFundsBtn);
+
+    /**
+     * Third step: done
+     */
+    const goToSwapBtn = await screen.findByRole("button", {
+      name: /Go to Swap/i,
+    });
+    expect(goToSwapBtn).toBeInTheDocument();
+    await user.click(goToSwapBtn);
+
+    /**
+     * Finished: go to swap
+     */
+    const swapBtn = await screen.findByRole("button", {
+      name: /Enter amount/i,
+    });
+    expect(swapBtn).toBeInTheDocument();
   });
 });
