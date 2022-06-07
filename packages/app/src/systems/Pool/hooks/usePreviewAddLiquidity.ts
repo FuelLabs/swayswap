4d25549da58eda412105cddd767ab84d13252985
Pedro Nauck <pedronauck@gmail.com>
2022-06-07 18:31:09 -0300

    fix: current pool position when there's no pool created

--- packages/app/src/systems/Pool/hooks/usePreviewAddLiquidity.ts
+++ packages/app/src/systems/Pool/hooks/usePreviewAddLiquidity.ts
@@ -18,8 +18,9 @@ export interface UsePreviewAddLiquidityProps {
 
 export function usePreviewAddLiquidity({ fromInput, poolInfo }: UsePreviewAddLiquidityProps) {
   const { totalLiquidity, poolTokensNum } = useUserPositions();
+  const amount = fromInput.amount || ZERO;
 
-  const liquidityFactor = (fromInput.amount || ZERO) * toBigInt(totalLiquidity);
+  const liquidityFactor = amount * toBigInt(totalLiquidity);
   const previewTokens = divideFnValidOnly(liquidityFactor, totalLiquidity);
   const nextTotalTokenSupply = previewTokens + toNumber(poolInfo?.lp_token_supply || BigInt(0));
   const nextCurrentPoolShare = divideFnValidOnly(
@@ -28,10 +29,15 @@ export function usePreviewAddLiquidity({ fromInput, poolInfo }: UsePreviewAddLiq
   );
 
   let formattedPreviewTokens = parseToFormattedNumber(previewTokens);
-  const formattedNextCurrentPoolShare = toFixed(nextCurrentPoolShare * 100);
+  let formattedNextCurrentPoolShare = toFixed(nextCurrentPoolShare * 100);
 
+  /**
+   * This is necessary for the first user interaction, when there's any
+   * pool already created. So, we need to set values manually.
+   */
   if (poolInfo?.eth_reserve === ZERO) {
-    formattedPreviewTokens = parseToFormattedNumber(fromInput.amount || 0);
+    formattedPreviewTokens = parseToFormattedNumber(amount);
+    formattedNextCurrentPoolShare = toFixed(amount > 0 ? 100 : 0);
   }
 
   return {
