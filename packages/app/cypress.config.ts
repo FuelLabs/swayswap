26f29b2909bd780cc7eb214dd2221524b7fbde0d
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-06-09 15:34:22 -0300

    fix

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    supportFile: 'packages/app/cypress/support/e2e.ts',
   },
   viewportWidth: 1280,
   viewportHeight: 720,
