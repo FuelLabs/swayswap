94658e43dda06d01bb43ea7283b91e3df17b92f7
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-06-09 16:26:39 -0300

    chore

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: 'packages/app/cypress/support/e2e.ts',
+    specPattern: 'packages/app/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
   viewportWidth: 1280,
   viewportHeight: 720,
