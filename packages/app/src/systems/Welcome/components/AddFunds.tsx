8b224f817f523381699986e5cc20a920491344fc
Pedro Nauck <pedronauck@gmail.com>
2022-06-09 03:25:54 -0300

    fix: missings add funds

--- packages/app/src/systems/Welcome/components/AddFunds.tsx
+++ packages/app/src/systems/Welcome/components/AddFunds.tsx
@@ -2,13 +2,14 @@ import { useWelcomeSteps } from "../hooks";
 
 import { WelcomeStep } from "./WelcomeStep";
 
+import { relativeUrl } from "~/systems/Core";
 import { FaucetApp } from "~/systems/Faucet";
 
 export function AddFunds() {
   const { next } = useWelcomeSteps();
   return (
     <WelcomeStep id={1}>
-      <img src="/illustrations/add-funds.png" />
+      <img src={relativeUrl("/illustrations/add-funds.png")} />
       <h2>Add some ETH to your wallet</h2>
       <p>
         To get started you&apos;ll need a wallet, <br />
