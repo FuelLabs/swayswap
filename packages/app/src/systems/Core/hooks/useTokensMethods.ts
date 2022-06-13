de489155f5905edff404f0dd81c040927c6991cd
Luiz Estacio <luizstacio@gmail.com>
2022-06-13 00:42:06 -0300

    feat: update mint page and contract types

--- packages/app/src/systems/Core/hooks/useTokensMethods.ts
+++ packages/app/src/systems/Core/hooks/useTokensMethods.ts
@@ -1,6 +1,5 @@
 import { useMemo } from 'react';
 
-import { objectId } from '../utils';
 import { getOverrides } from '../utils/gas';
 
 import { useWallet } from './useWallet';
@@ -19,15 +18,16 @@ export function useTokenMethods(tokenId: string) {
     getBalance() {
       return wallet?.getBalance(tokenId);
     },
-    async queryNetworkFee(amount: bigint) {
-      return contract.prepareCall.mint_and_transfer_coins(amount, objectId(wallet.address), {
+    async queryNetworkFee() {
+      return contract.prepareCall.mint({
         variableOutputs: 1,
       });
     },
-    mint(amount: bigint, gasLimit: bigint) {
-      return contract.submit.mint_and_transfer_coins(
-        amount,
-        objectId(wallet.address),
+    async getMintAmount() {
+      return contract.dryRun.get_mint_amount();
+    },
+    mint(gasLimit: bigint) {
+      return contract.submit.mint(
         getOverrides({
           variableOutputs: 1,
           gasLimit,
