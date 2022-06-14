03202c3a515ed362256111f65e442f066df42d05
Luiz Estacio <luizstacio@gmail.com>
2022-06-14 20:50:52 -0300

    feat: add legal agreement

--- packages/app/src/systems/Welcome/components/WelcomeDone.tsx
+++ packages/app/src/systems/Welcome/components/WelcomeDone.tsx
@@ -1,31 +1,55 @@
-import { useNavigate } from "react-router-dom";
-
 import { useWelcomeSteps } from "../hooks";
 
 import { WelcomeStep } from "./WelcomeStep";
 
 import { relativeUrl } from "~/systems/Core";
-import { Button } from "~/systems/UI";
+import { Button, Link } from "~/systems/UI";
+
+const DISCLAIMER_URL =
+  "https://github.com/FuelLabs/swayswap/blob/master/docs/LEGAL_DISCLAIMER.md";
 
 export function WelcomeDone() {
-  const navigate = useNavigate();
-  const { send } = useWelcomeSteps();
+  const { send, state } = useWelcomeSteps();
 
   function handleDone() {
-    navigate("/swap");
     send("FINISH");
   }
 
   return (
     <WelcomeStep id={2}>
       <img src={relativeUrl("/illustrations/done.png")} width="70%" />
-      <h2>Done, congrats!</h2>
+      <h2>Wallet Created!</h2>
       <p className="my-5">
-        Now you are done to swap your coins using the fatest modular execution
-        layer
+        Now you&apos;re ready to swap and pool <b>test assets</b> using Fuel:
+        the fastest modular execution layer.
       </p>
-      <Button size="lg" variant="primary" onPress={handleDone}>
-        Go to Swap
+      <div className="flex justify-center items-center mb-6">
+        <input
+          checked={state.context.acceptAgreement}
+          onChange={(e) => {
+            send("ACCEPT_AGREEMENT", {
+              value: e.target.checked,
+            });
+          }}
+          className="h-5 w-5 mr-3 cursor-pointer"
+          type="checkbox"
+        />
+        <span>
+          {" "}
+          I agree to the{" "}
+          <Link isExternal href={DISCLAIMER_URL}>
+            (legal disclaimer)
+          </Link>
+          .
+        </span>
+      </div>
+      <Button
+        size="lg"
+        variant="primary"
+        isDisabled={!state.context.acceptAgreement}
+        onPress={handleDone}
+      >
+        Get Swapping!
       </Button>
     </WelcomeStep>
   );
