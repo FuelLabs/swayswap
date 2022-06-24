0c88e6ed8817893d068c3410d044946f94545ccc
Luiz Estácio | stacio.eth <luizstacio@gmail.com>
2022-06-24 17:11:19 -0300

    fix: avoid resize during image loading (#349)

--- packages/app/src/systems/Welcome/components/AddFunds.tsx
+++ packages/app/src/systems/Welcome/components/AddFunds.tsx
@@ -1,15 +1,15 @@
 import { useWelcomeSteps } from "../hooks";
 
+import { WelcomeImage } from "./WelcomeImage";
 import { WelcomeStep } from "./WelcomeStep";
 
-import { relativeUrl } from "~/systems/Core";
 import { FaucetApp } from "~/systems/Faucet";
 
 export function AddFunds() {
   const { next } = useWelcomeSteps();
   return (
     <WelcomeStep id={1}>
-      <img src={relativeUrl("/illustrations/add-funds.png")} />
+      <WelcomeImage src="/illustrations/add-funds.png" />
       <h2>Add some test ETH to your wallet</h2>
       <p>
         To get started you&apos;ll need some funds.
