2d40d84024703de17b475e34c9b991fb6c62fff7
Luiz Estacio <luizstacio@gmail.com>
2022-06-24 16:34:33 -0300

    fix: avoid resize during image loading

--- packages/app/src/systems/Welcome/components/CreateWallet.tsx
+++ packages/app/src/systems/Welcome/components/CreateWallet.tsx
@@ -2,9 +2,10 @@ import { useMutation } from "react-query";
 
 import { useWelcomeSteps } from "../hooks";
 
+import { WelcomeImage } from "./WelcomeImage";
 import { WelcomeStep } from "./WelcomeStep";
 
-import { relativeUrl, useAppContext } from "~/systems/Core";
+import { useAppContext } from "~/systems/Core";
 import { Button } from "~/systems/UI";
 
 export function CreateWallet() {
@@ -23,7 +24,7 @@ export function CreateWallet() {
 
   return (
     <WelcomeStep id={0}>
-      <img src={relativeUrl("/illustrations/create-wallet.png")} />
+      <WelcomeImage src="/illustrations/create-wallet.png" />
       <h2>Welcome to SwaySwap</h2>
       <p className="my-5">
         To get started you&apos;ll need a wallet.
