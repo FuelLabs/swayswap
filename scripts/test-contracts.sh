5e8d48d59cdf91c46a40bdc4cb7ed201569eac2f
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-10-13 20:24:00 -0300

    chore

--- scripts/test-contracts.sh
+++ scripts/test-contracts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 CURRENT_PATH=$(pwd)
-# cd $CURRENT_PATH/packages/contracts/token_contract && forc test
-# cd $CURRENT_PATH/packages/contracts/exchange_contract && forc test
+cd $CURRENT_PATH/packages/contracts/token_contract && forc test
+cd $CURRENT_PATH/packages/contracts/exchange_contract && forc test
