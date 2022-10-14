bfca5c953550b6dbea2e0206f9216c05e44cb2b5
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2022-10-13 21:31:16 -0300

    fix

--- scripts/test-contracts.sh
+++ scripts/test-contracts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# CURRENT_PATH=$(pwd)
+CURRENT_PATH=$(pwd)
 cd $CURRENT_PATH/packages/contracts/token_contract && cargo test
 cd $CURRENT_PATH/packages/contracts/exchange_contract && cargo test
