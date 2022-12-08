be46d7ed494ed0b8d8f832ac5dd51582e76232c1
luizstacio <luizstacio@gmail.com>
2022-12-08 09:56:27 -0800

    fix: await faucet

--- packages/app/src/systems/Faucet/hooks/__mocks__/useFaucet.ts
+++ packages/app/src/systems/Faucet/hooks/__mocks__/useFaucet.ts
@@ -2,21 +2,16 @@ import type { Wallet } from 'fuels';
 
 import { fetchFaucet } from '../useFaucet';
 
-import { sleep } from '~/systems/Core';
-
 export async function faucet(wallet: Wallet, times = 2) {
   const range = Array.from({ length: times });
-  await Promise.all(
-    range.map(async (n, idx) => {
-      await sleep(200 * idx);
-      const res = await fetchFaucet({
-        method: 'POST',
-        body: JSON.stringify({
-          address: wallet?.address.toAddress(),
-          captcha: '',
-        }),
-      });
-      return res;
-    })
-  );
+  await range.reduce(async (promise) => {
+    await promise;
+    return fetchFaucet({
+      method: 'POST',
+      body: JSON.stringify({
+        address: wallet?.address.toAddress(),
+        captcha: '',
+      }),
+    });
+  }, Promise.resolve());
 }
