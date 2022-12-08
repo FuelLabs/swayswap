7a3f70e29b9090a6b677a6e03a5a70529d143fb1
Luiz Estácio | stacio.eth <luizstacio@gmail.com>
2022-12-08 10:08:02 -0800

    feat: remove twitter button (#394)

--- packages/app/src/systems/Core/components/ActionsWidget.tsx
+++ packages/app/src/systems/Core/components/ActionsWidget.tsx
@@ -1,13 +1,7 @@
-import { BsTwitter } from "react-icons/bs";
 import { FaFaucet } from "react-icons/fa";
 
 import { FaucetDialog, useFaucetDialog } from "~/systems/Faucet";
-import { Button, Link } from "~/systems/UI";
-
-const tweetText = `I'm using #SwaySwap, a blazingly fast DEX on the Fuel devnet @fuellabs_\nhttps://fuellabs.github.io/swayswap`;
-const tweetLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-  tweetText
-)}`;
+import { Button } from "~/systems/UI";
 
 export function ActionsWidget() {
   const faucetDialog = useFaucetDialog();
@@ -23,15 +17,6 @@ export function ActionsWidget() {
         <FaFaucet />
         <span className="content">Faucet</span>
       </Button>
-      <Link href={tweetLink} isExternal className="no-underline">
-        <Button
-          size="md"
-          className="actionsWidget--btn actionsWidget--shareBtn"
-        >
-          <BsTwitter />
-          <span className="content">Share</span>
-        </Button>
-      </Link>
       <FaucetDialog />
     </div>
   );
