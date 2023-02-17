d39d393f419f0a0015fb6c598ae901b5919b156c
Matt Auer <mattauer@umich.edu>
2023-02-17 10:52:00 -0600

    chore: remove commented code

--- packages/app/src/systems/Core/context.tsx
+++ packages/app/src/systems/Core/context.tsx
@@ -1,22 +1,11 @@
-// import { Wallet } from "fuels";
-// import { useAtom } from "jotai";
-// import { atomWithStorage } from "jotai/utils";
 import type { ReactNode } from "react";
 import React, { useContext } from "react";
 
-// import { LocalStorageKey } from "./utils";
-
-// import { FUEL_PROVIDER_URL } from "~/config";
 import type { Maybe } from "~/types";
 
 interface AppContextValue {
   justContent?: boolean;
 }
-
-// const walletPrivateKeyState = atomWithStorage<Maybe<string>>(
-//   `${LocalStorageKey}-state`,
-//   null
-// );
 
 export const AppContext = React.createContext<Maybe<AppContextValue>>(null);
 
@@ -31,13 +20,6 @@ export const AppContextProvider = ({
   justContent,
   children,
 }: ProviderProps) => {
-  // const [privateKey, setPrivateKey] = useAtom(walletPrivateKeyState);
-
-  // const wallet = useMemo(() => {
-  //   if (!privateKey) return null;
-  //   return new Wallet(privateKey, FUEL_PROVIDER_URL);
-  // }, [privateKey]);
-
   return (
     <AppContext.Provider
       value={{
