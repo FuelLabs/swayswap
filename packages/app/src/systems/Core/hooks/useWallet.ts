3a2d0d258f0b77d970c5be1f3f19e91329ba234b
Matt Auer <mattauer@umich.edu>
2023-02-20 18:23:57 -0600

    refactor: remove automatic connection and add comments

--- packages/app/src/systems/Core/hooks/useWallet.ts
+++ packages/app/src/systems/Core/hooks/useWallet.ts
@@ -12,10 +12,9 @@ export const useWallet = () => {
   } = useQuery(
     ['wallet'],
     async () => {
-      const isConnected = await fuel?.isConnected();
-      if (!isConnected) {
-        // await fuel?.connect();
-      }
+      // The wallet should be connected as the user did it in the first step
+      // We could add a check to see if the wallet is past the welcome steps
+      // and still disconnected
       const currentAccount = await fuel!.currentAccount();
       const currentWallet = (await fuel?.getWallet(currentAccount))!;
       return currentWallet;
