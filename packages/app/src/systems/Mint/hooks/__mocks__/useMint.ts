f04ed5cb0f1aef25a4c71342a72aebc1465cedc1
Matt Auer <mattauer@umich.edu>
2023-03-16 17:34:47 -0500

    refactor: mock mint

--- packages/app/src/systems/Mint/hooks/__mocks__/useMint.ts
+++ packages/app/src/systems/Mint/hooks/__mocks__/useMint.ts
@@ -1,13 +1,17 @@
 import type { FuelWalletLocked } from '@fuel-wallet/sdk';
 
-import { TOKEN_ID2 } from '~/config';
+import { TOKEN_ID1, TOKEN_ID2 } from '~/config';
 import { getOverrides } from '~/systems/Core/utils/gas';
 import { TokenContractAbi__factory } from '~/types/contracts';
 
 export async function mint(wallet: FuelWalletLocked) {
-  const contract = TokenContractAbi__factory.connect(TOKEN_ID2, wallet);
-  return contract.functions
-    .mint()
-    .txParams(getOverrides({ variableOutputs: 1 }))
+  const tokenContract1 = TokenContractAbi__factory.connect(TOKEN_ID1, wallet);
+  const tokenContract2 = TokenContractAbi__factory.connect(TOKEN_ID2, wallet);
+
+  return tokenContract1
+    .multiCall([
+      tokenContract1.functions.mint().txParams(getOverrides({ variableOutputs: 1 })),
+      tokenContract2.functions.mint().txParams(getOverrides({ variableOutputs: 1 })),
+    ])
     .call();
 }
