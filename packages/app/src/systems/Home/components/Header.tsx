b137ccf65c6600afeb479cf18473e988f78763b0
Matt Auer <mattauer@umich.edu>
2023-03-29 09:46:48 -0500

    refactor: use new logo

--- packages/app/src/systems/Home/components/Header.tsx
+++ packages/app/src/systems/Home/components/Header.tsx
@@ -1,3 +1,4 @@
+import { FuelLogo } from "@fuel-ui/react";
 import { useNavigate } from "react-router-dom";
 
 import { relativeUrl, useBreakpoint } from "~/systems/Core";
@@ -9,7 +10,7 @@ export function Header() {
   const navigate = useNavigate();
   return (
     <header className="homePage--header">
-      <img src={relativeUrl("/fuel-logo-512x512.png")} alt="swayswap" />
+      <FuelLogo />
       <nav className="homePage--menu">
         <Link isExternal href="https://github.com/FuelLabs/swayswap">
           Github
